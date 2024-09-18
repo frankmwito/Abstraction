615185307cd6be262c54848ad9ac9b6a975bce9f
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-18 10:51:03 +0300

    Abstract Appliance System

--- Abstract appliance system.py
+++ Abstract appliance system.py
@@ -1,2 +1,51 @@
 # Abstract Appliance System
 
+from abc import ABC, abstractmethod
+
+# Abstract Appliance Class
+class Appliance(ABC):
+    @abstractmethod
+    def turn_on(self):
+        pass
+    
+    @abstractmethod
+    def turn_off(self):
+        pass
+
+# Washing Machine Class
+class WashingMachine(Appliance):
+    def turn_on(self):
+        return "The washing machine has electrical issues"
+    
+    def turn_off(self):
+        return "The washing machine does a good job"
+
+# Refrigerator Class
+class Refrigerator(Appliance):
+    def turn_on(self):
+        return "The refrigerator is in good working condition"
+    
+    def turn_off(self):
+        return "The refrigerator has a problem"
+
+# Air Conditioner Class
+class AirConditioner(Appliance):
+    def turn_on(self):
+        return "The air conditioner is working"
+    
+    def turn_off(self):
+        return "The air conditioner is not working"
+
+# Create instances and call methods
+appliance1 = WashingMachine()
+print(appliance1.turn_on())
+print(appliance1.turn_off())
+
+appliance2 = Refrigerator()
+print(appliance2.turn_on())
+print(appliance2.turn_off())
+
+appliance3 = AirConditioner()
+print(appliance3.turn_on())  # Fixed: Added parentheses
+print(appliance3.turn_off()) # Fixed: Added parentheses
+  
